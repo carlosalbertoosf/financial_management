38622cb0ac7c96aefb1491dcf35b46dec459e7dd
carlosalbertoosf <carlosalbertoosf@gmail.com>
2026-08-27 11:38:39 -0300

    feat: add HATEOAS support to user service

--- src/main/java/com/carlosalbertoosf/personal_finance_api/services/UserServices.java
+++ src/main/java/com/carlosalbertoosf/personal_finance_api/services/UserServices.java
@@ -1,5 +1,6 @@
 package com.carlosalbertoosf.personal_finance_api.services;
 
+import com.carlosalbertoosf.personal_finance_api.controllers.UserController;
 import com.carlosalbertoosf.personal_finance_api.data.dto.request.UserRequestDTO;
 import com.carlosalbertoosf.personal_finance_api.data.dto.response.UserResponseDTO;
 import com.carlosalbertoosf.personal_finance_api.model.User;
@@ -7,6 +8,8 @@ import com.carlosalbertoosf.personal_finance_api.repository.UserRepository;
 import static com.carlosalbertoosf.personal_finance_api.mapper.ObjectMapper.parseObject;
 import static com.carlosalbertoosf.personal_finance_api.mapper.ObjectMapper.parseListObjects;
 import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,14 +21,18 @@ public class UserServices {
     private UserRepository userRepository;
 
     public List<UserResponseDTO> findAll() {
-        return parseListObjects(userRepository.findAll(), UserResponseDTO.class);
+        var users = parseListObjects(userRepository.findAll(), UserResponseDTO.class);
+        users.forEach(this::addHateoasLinks);
+        return users;
     }
 
     public UserResponseDTO findById(Long id) {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
-        return parseObject(user, UserResponseDTO.class);
+        var responseDTO = parseObject(user, UserResponseDTO.class);
+        addHateoasLinks(responseDTO);
+        return responseDTO;
     }
 
     public UserResponseDTO create(UserRequestDTO userDTO) {
@@ -33,7 +40,9 @@ public class UserServices {
 
         User userSaved = userRepository.save(user);
 
-        return parseObject(userSaved, UserResponseDTO.class);
+        var responseDTO = parseObject(userSaved, UserResponseDTO.class);
+        addHateoasLinks(responseDTO);
+        return responseDTO;
     }
 
     public UserResponseDTO update(Long id, UserRequestDTO userDTO) {
@@ -44,7 +53,9 @@ public class UserServices {
         user.setEmail(userDTO.getEmail());
         user.setPassword(userDTO.getPassword());
 
-        return parseObject(userRepository.save(user), UserResponseDTO.class);
+        var responseDTO = parseObject(userRepository.save(user), UserResponseDTO.class);
+        addHateoasLinks(responseDTO);
+        return responseDTO;
     }
 
     public void delete(Long id) {
@@ -52,5 +63,32 @@ public class UserServices {
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
         userRepository.delete(user);
+    }
+
+    private void addHateoasLinks(UserResponseDTO dto) {
+        dto.add(linkTo(methodOn(UserController.class)
+                .findById(dto.getId()))
+                .withSelfRel()
+                .withType("GET"));
+
+        dto.add(linkTo(methodOn(UserController.class)
+                .findAll())
+                .withRel("findAll")
+                .withType("GET"));
+
+        dto.add(linkTo(methodOn(UserController.class)
+                .create(null))
+                .withRel("create")
+                .withType("POST"));
+
+        dto.add(linkTo(methodOn(UserController.class)
+                .update(dto.getId(), null))
+                .withRel("update")
+                .withType("PUT"));
+
+        dto.add(linkTo(methodOn(UserController.class)
+                .delete(dto.getId()))
+                .withRel("delete")
+                .withType("DELETE"));
     }
 }
